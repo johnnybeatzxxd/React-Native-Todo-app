db756834f95a9e8204ca0bcceddc782ec89cfda9
johnnybeatzxxd <johnnybeatzxxd@gmail.com>
2024-05-30 22:19:56 +0300

    made the add button functional

--- App.js
+++ App.js
@@ -1,9 +1,18 @@
 import { ScrollView, StyleSheet, Text, View, Image, TextInput, Button, SafeAreaView, Platform, StatusBar, TouchableOpacity } from 'react-native';
 import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
 
 
 export default function App() {
   const [text, onChangeText] = React.useState('');
+  const [todos, setTodos] = React.useState([]);
+  function AddTodo(){
+    text != '' ? setTodos(text) : null
+    console.log(todos);
+    onChangeText("");
+  }
+
   return (
    <View style={styles.body}>
     <StatusBar />
@@ -16,7 +25,16 @@ export default function App() {
             onChangeText={onChangeText}
             placeholder='Todo'
           />
-          <Button title='Add'/>
+          <Button title='Add' onPress={AddTodo}/>
+        </View>
+        <View style={{marginTop:30}}>
+          {todos.map((todo,idx)=>(
+            <View  key={idx} style={styles.todo}>
+              <Text >{todo}</Text>
+              <Button title='delete' style={{width:50}} onPress={()=>todos.splice(idx,1)}/>
+            </View>
+            ))}
+          
         </View>
       </View>
    </View>
@@ -47,4 +65,11 @@ const styles = StyleSheet.create({
     padding: 10,
     width:250,
   },
+  todo:{
+    flexDirection : 'row',
+    justifyContent:'space-between',
+    fontSize:20,
+    paddingTop:20,
+    paddingRight:20
+  }
 });
