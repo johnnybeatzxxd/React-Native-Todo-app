09d2e5f4e1cbede3c1ffad1770a6f6ec9f5646fd
johnnybeatzxxd <johnnybeatzxxd@gmail.com>
2024-05-30 21:10:57 +0300

    made the input field

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View, Image, TextInput, Button, SafeAreaView, Platform, StatusBar, TouchableOpacity } from 'react-native';
+import React from 'react';
+
 
 export default function App() {
+  const [text, onChangeText] = React.useState('');
   return (
-    <View style={styles.container}>
-      <Text>hellow world!</Text>
-      <StatusBar style="auto" />
-    </View>
+   <View style={styles.body}>
+    <StatusBar />
+      <View style={styles.App}>
+        <Text style={{fontSize:30}}>Todo List</Text>
+        <View style={styles.input}>
+          <TextInput 
+            value={text} 
+            style={styles.inputField} 
+            onChangeText={onChangeText}
+            placeholder='Todo'
+          />
+  
+        </View>
+      </View>
+   </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
+  body:{
+    flex:1,
+    flexDirection:'column',
+    justifyContent: "start",
+    alignItems: "start",
+    backgroundColor:"lightgrey",
+  },
+  App:{
+    padding:20,
+    top:40,
+    left:20,
+  },
+  input:{
+    marginTop: 20,
+    justifyContent:'start',
+    flexDirection:'row',
+  },
+  inputField: {
+    height: 40,
+    borderWidth: 0.5,
+    padding: 10,
+    width:250,
   },
 });
